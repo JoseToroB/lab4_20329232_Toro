adb96b439997d18b54ac04ee0c11f30aea031d72
JoseToroB <65199659+JoseToroB@users.noreply.github.com>
2021-08-21 23:53:08 -0400

    Add files via upload

---
+++ Modelo/Comentario.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelo
+{
+    /// <summary>
+    /// La clase Comentario contiene todas las caracteristicas de un comentario
+    /// </summary>
+    public class Comentario
+    {
+        //Atributos
+        private static int idContador = 0;
+        private Usuario autor;
+        private string fecha;
+        private string contenido;
+        private int id;
+        private int likes;
+        private List<Comentario> comentarios;
+
+        //Constructor
+        /// <summary>
+        /// Constructor de una respuesta, recibe el contenido a publicar.
+        /// </summary>
+        public Comentario(Usuario autor,string contenido)
+        {
+            this.autor = autor;
+            this.contenido = contenido;
+            idContador += 1;
+            this.id = idContador;
+            this.likes = 0;
+            this.comentarios = new List<Comentario>();
+            //obtener fecha
+            DateTime fecha = DateTime.Now;
+            this.fecha = fecha.ToString("dd'/'MM'/'yyyy");
+        }
+
+        //Getters y setters
+        /// <value> Devuelve o setea el valor del contador autoincremental </value>
+        public static int IdContador { get => idContador; set => idContador = value; }
+        /// <value> Devuelve o setea al autor </value>
+        public Usuario Autor { get => autor; set => autor = value; }
+        /// <value> Devuelve o setea la fecha  </value>
+        public string Fecha { get => fecha; set => fecha = value; }
+        /// <value> Devuelve o setea el contenido </value>
+        public string Contenido { get => contenido; set => contenido = value; }
+        /// <value> Devuelve o setea el id </value>
+        public int Id { get => id; set => id = value; }
+        /// <value> Devuelve o setea los likes </value>
+        public int Likes { get => likes; set => likes = value; }
+        /// <value> Devuelve o setea la lista de comentarios </value>
+        public List<Comentario> Comentarios { get => comentarios; set => comentarios = value; }
+    }
+}
---
+++ Modelo/RedS.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelo
+{
+    /// <summary>
+    /// La clase RS contiene las caracteristicas de la red social
+    /// </summary>
+    public class RedS
+    {
+        //Atributos
+        private List<Publicacion> publis;
+        private List<Usuario> usuarios;
+        private List<Comentario> comentarios;
+        private Usuario usuarioConectado;
+        private bool conectado = false;
+
+        //Constructor
+        /// <summary>
+        /// Constructor de un Stack, inicializa listas para almacenar datos.
+        /// </summary>
+        public RedS()
+        {
+            this.publis = new List<Publicacion>();
+            this.usuarios = new List<Usuario>();
+            this.comentarios = new List<Comentario>();
+        }
+
+        //Getters y Setters
+        /// <value> Devuelve o setea la lista de preguntask </value>
+        public List<Publicacion> Publis { get => publis; set => publis= value; }
+        /// <value> Devuelve o setea la lista de usuarios  </value>
+        public List<Usuario> Usuarios { get => usuarios; set => usuarios = value; }
+        /// <value> Devuelve o setea al usuario conectado </value>
+        public Usuario UsuarioConectado { get => usuarioConectado; set => usuarioConectado = value; }
+        /// <value> Devuelve o setea el valor de si hay alguien conectado  </value>
+        public bool Conectado { get => conectado; set => conectado = value; }
+        /// <value> Devuelve o setea la lista de comentarios</value>
+        public List<Comentario> Comentarios { get => comentarios; set => comentarios = value; }
+    }
+}
